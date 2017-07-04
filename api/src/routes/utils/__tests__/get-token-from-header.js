bb9f8de017841666556c0c3aa7ab45ef41a5fc9d
Blake Schwartz <blakehschwartz@gmail.com>
2017-07-04 13:22:11 -0700

    exercise 2

--- api/src/routes/utils/__tests__/get-token-from-header.js
+++ api/src/routes/utils/__tests__/get-token-from-header.js
@@ -1,22 +1,28 @@
 import getTokenFromHeader from '../get-token-from-header'
 
 test('getTokenFromHeader returns null if there is no token', () => {
-  const result = getTokenFromHeader({headers: {}})
+  const req = getReq()
+  const result = getTokenFromHeader(req)
   expect(result).toBe(null)
 })
 
 test('getTokenFromHeader returns the token from the headers', () => {
   const token = 'hi.mom!'
-  const authHeader = `Token ${token}`
-  const req = {
-    headers: {
-      authorization: authHeader,
-    },
-  }
+  const authorization = `Token ${token}`
+  const req = getReq(authorization)
 
   const result = getTokenFromHeader(req)
   expect(result).toBe(token)
 })
+
+function getReq(authorization) {
+  return {
+    headers: {
+      authorization,
+    },
+  }
+}
+
 test('I submitted my elaboration and feedback', () => {
   const submitted = true
   expect(true).toBe(submitted)
